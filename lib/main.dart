c759d13620d2d0b753441a3e8f90358f96d3f2e4
jiniusz <dev.jiniusz@gmail.com>
2022-03-17 14:20:53 +0700

    feat: completed Column & Row

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter App'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              child: Card(
+                child: Text('CHART!'),
+                color: Colors.blue,
+                elevation: 5,
+              ),
+              width: double.infinity,
+            ),
+            Card(
+              child: Text('List of TX'),
+              color: Colors.red,
+            ),
+          ],
+        ));
+  }
+}
