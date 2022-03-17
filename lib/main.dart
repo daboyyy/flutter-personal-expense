25e58887471642ab30f500f33da6c5ae4b13211f
jiniusz <dev.jiniusz@gmail.com>
2022-03-17 15:08:58 +0700

    feat: completed time to styles

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './transaction.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -18,6 +20,16 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: 't2',
+        title: 'Weekly Greceries',
+        amount: 16.53,
+        date: DateTime.now())
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,18 +40,40 @@ class MyHomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              child: Card(
-                child: Text('CHART!'),
-                color: Colors.blue,
-                elevation: 5,
-              ),
-              width: double.infinity,
-            ),
             Card(
-              child: Text('List of TX'),
-              color: Colors.red,
+              child: Text('CHART!'),
+              color: Colors.blue,
             ),
+            Column(
+              children: transactions.map((tx) {
+                return Card(
+                    child: Row(
+                  children: <Widget>[
+                    Container(
+                      child: Text(
+                        tx.amount.toString(),
+                        style: TextStyle(
+                            color: Colors.purple,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20),
+                      ),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.purple, width: 2)),
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      padding: EdgeInsets.all(10),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(tx.title, style: TextStyle(color: Colors.black, fontSize: 16),),
+                        Text(tx.date.toString(), style: TextStyle(color: Colors.grey))
+                      ],
+                    )
+                  ],
+                ));
+              }).toList(),
+            )
           ],
         ));
   }
